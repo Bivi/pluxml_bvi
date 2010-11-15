f6e49262e5d006ca1ea43afc3f4b6aa6485dd6b5
Benoit Vincent <bvi.perso@gmail.com>
2010-11-16 00:03:35 +0100

    Modification thème theme-bvi
    * Ajout d'une page archives
    * Reformatage du code
    * Remontée de la date dans le titre de l'article
    * Ajout d'un bouton [editer] dans la page d'article

--- themes/theme-bvi/article.php
+++ themes/theme-bvi/article.php
@@ -1,23 +1,25 @@
-<?php include(dirname(__FILE__).'/header.php'); # On insere le header ?>
-	<div class="post">
-		<h3 class="title"><?php $plxShow->artTitle('link'); ?></h3>
+<?php include(dirname(__FILE__) . '/header.php'); # On insere le header  ?>
+<div class="post">
+  <h3 class="title"><?php $plxShow->artTitle('link'); ?> (<?php $plxShow->artDate('#num_day/#num_month/#num_year(4)'); ?>)</h3>
 
-		<div class="content">
-			<?php $plxShow->artContent(); ?>
-			<div class="fixed"></div>
-		</div>
+  <div class="content">
+    <?php $plxShow->artContent(); ?>
+    <div class="fixed"></div>
+  </div>
 
-		<div class="meta">
-				<!-- <div class="author"></div> //Option pour gravatar -->
+  <div class="meta">
+    <!-- <div class="author"></div> //Option pour gravatar -->
 
-			<div class="act">
-				<!-- <a href="#respond">Laisser un Commentaire</a> -->
-			</div>
-			<div class="info">
-				Le <?php $plxShow->artDate('<span>#num_day</span> #num_month #num_year(4)'); ?> | Class&eacute; dans : <?php $plxShow->artCat(); ?>
-			</div>
-			<div class="fixed"></div>
-		</div>
-	</div>
-		<?php include(dirname(__FILE__).'/commentaires.php'); # On insere les commentaires ?>
-<?php include(dirname(__FILE__).'/footer.php'); # On insere le footer ?>
+    <div class="act">
+      <!-- <a href="#respond">Laisser un Commentaire</a> -->
+    </div>
+    <div class="info">
+      Class&eacute; dans : <?php $plxShow->artCat(); ?>
+      <?php echo '&nbsp;    [<a href="core/admin/article.php?a='.$plxShow->plxMotor->plxRecord_arts->f('numero').'" title="&Eacute;diter cet article" target="_blank">Editer</a>]' ?>
+      <p class="info_bottom">Mots cl&eacute;s : <?php $plxShow->artTags(); ?></p>
+    </div>
+    <div class="fixed"></div>
+  </div>
+</div>
+<?php include(dirname(__FILE__) . '/commentaires.php'); # On insere les commentaires ?>
+<?php include(dirname(__FILE__) . '/footer.php'); # On insere le footer ?>
